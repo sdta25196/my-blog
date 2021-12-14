1bd8598b57a73f34331039dc5737b0ca60eb3022
sdta25196 <451376882@qq.com>
2021-12-14 15:52:12 +0800

    修改稳住

--- src/assets/static/translate.js
+++ src/assets/static/translate.js
@@ -4,7 +4,7 @@ const translate = [{
         fileName: "e919341c71771ea9dc14ad522ad2c7eda5227db9",
         title: "Activating Prior Knowledge With English Language Learners",
         filePath: "/blogs/translate/",
-        date: "2021/10/13 上午11:40:36",
+        date: "2021/12/6 下午10:29:46",
         description: "## Activating Prior Knowledge With English Language Learners@@@@@@> 激活英语学习者的先验知识@@@@@@Activating prior knowledge means both eliciting from students what they already know and building initial knowledge that they need in order to access upcoming content.",
         outline: ["## Activating Prior Knowledge With English Language Learners"],
       },{
@@ -12,7 +12,7 @@ const translate = [{
         fileName: "1e25be1674c4cd784c50192744e34bf411a05d7c",
         title: "how does JavaScript Work",
         filePath: "/blogs/translate/",
-        date: "2021/10/13 下午5:21:28",
+        date: "2021/12/6 下午10:29:46",
         description: "## How does JavaScript Work?@@@@@@> JavaScript是如何工作的@@@@@@JavaScript is a client-side scripting language and one of the most efficient, commonly used scripting languages. The term .client-side scripting language means that it runs at the client-side( or on the client machine) inside the web-browsers, but one important thing to remember is that client's web-browser also needs to support the JavaScript or it must be JavaScript enabled. Nowadays, most of the modern web browsers support JavaScript and have their JavaScript engines. For example, Google Chrome has its own JavaScript engine called V8.",
         outline: ["## How does JavaScript Work?","## Introduction ","### Step 1: Parser","### Step 2: AST","### Step 3: Conversion to Machine code","### Step 4: Machine code"],
       },{
@@ -20,7 +20,7 @@ const translate = [{
         fileName: "2e76d0fdfc253b0eda8b4b7493bf05ae2dc38b97",
         title: "What Is Clean Code",
         filePath: "/blogs/translate/",
-        date: "2021/10/4 下午1:45:32",
+        date: "2021/12/6 下午10:29:46",
         description: "## What Is Clean Code?@@@@@@> 什么是简洁代码@@@@@@Clean code is a philosophy that I follow when I code. Actually, to me, it feels more like a religion than a philosophy! It’s the idea that your code should be precise and as close to perfect as possible. If you have more code than you need, it shouldn’t be there. There shouldn’t be anything superfluous, and I go so far as to say that there shouldn’t even be superfluous whitespace. You want your code to be as efficient, readable, and maintainable as possible, and instead of only “solving the problem.” I always put in the extra time to also focus on the design of my code.",
         outline: ["## What Is Clean Code?","## How to Implement Clean Coding Design","## Estimation Gambling","## System Design","## Writing the Code"],
       }]
