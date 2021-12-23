a3f8580a7ca56f8191179c5d48641a21da87a0ee
sdta25196 <451376882@qq.com>
2021-12-23 22:29:00 +0800

    新文章

--- src/assets/static/article.js
+++ src/assets/static/article.js
@@ -4,7 +4,7 @@ const article = [{
         fileName: "68f85cdeb258a15044b3236917b43f46344439e6",
         title: "Event Loop - 事件循环详解",
         filePath: "/blogs/article/",
-        date: "2021/10/4 下午1:34:25",
+        date: "2021/12/6 下午10:29:46",
         description: "## 事件循环@@@  @@@事件循环的本质就是JavaScript引擎在 **等待任务**、**执行任务**、**进入休眠状态等待更多任务**这几个状态之间转换的无限循环。@@@@@@> 出现任务 —— 引擎处理它们 —— 等待更多任务（即休眠，几乎不消耗 CPU 资源）—— 出现新的任务",
         outline: ["## 事件循环","## 浏览器中的宏任务","## 浏览器中的微任务","## 总结 & 补充"],
       },{
@@ -12,7 +12,7 @@ const article = [{
         fileName: "01314109be59f26a60126c86b50940a7ace8e1ae",
         title: "GC-垃圾回收机制",
         filePath: "/blogs/article/",
-        date: "2021/10/4 下午1:34:25",
+        date: "2021/12/6 下午10:29:46",
         description: "## 垃圾回收@@@@@@  c一类的语言拥有底层的内存管理原语`malloc()`和`free()`，@@@  而像javascript这样的高级语言，会自动释放内存，这一过程就是**内存垃圾回收**@@@  ",
         outline: ["## 垃圾回收","## 内存生命周期","## JavaScript 中的分配内存","## JavaScript 中的使用内存","## JavaScript 中的释放内存","## 万物都有其两面性","## 四种常见的javascript内存泄露","## 管理内存的方式"],
       },{
@@ -20,7 +20,7 @@ const article = [{
         fileName: "b1033978ebd32f49f8b4a6f46e4ca7c8689f71f8",
         title: "html渲染原理及其优化思路",
         filePath: "/blogs/article/",
-        date: "2021/10/7 下午3:19:22",
+        date: "2021/12/6 下午10:29:46",
         description: "## HTML的解析过程@@@@@@![7tz9q1ngpp](assets/7tz9q1ngpp/../7tz9q1ngpp.jpg)@@@@@@网络进程接收到响应头之后，会根据响应头中的 content-type 字段来判断文件的类型，比如 content-type 的值是“text/html”，那么浏览器就会判断这是一个 HTML 类型的文件，然后为该请求选择或者创建一个渲染进程，渲染进程准备好之后，网络进程和渲染进程之间会建立一个共享数据的管道，网络进程接收到数据后就往这个管道里面放，而渲染进程则从管道的另外一端不断地读取数据，并同时将读取的数据“喂”给 HTML 解析器。你可以把这个管道想象成一个“水管”，网络进程接收到的字节流像水一样倒进这个“水管”，而“水管”的另外一端是渲染进程的 HTML 解析器，它会动态接收字节流，并将其解析为 DOM。如下图",
         outline: ["## HTML的解析过程","### dom的解析","### CSS的解析","### 渲染树","#### 渲染步骤","###  回流与重绘","#### reflow与repaint的时机","### CSS的渲染阻塞","## 关键渲染路径","### 关键路径渲染和阻塞渲染","### css具体如何阻塞资源的","### JavaScript具体如何阻塞资源的","### 改变脚本加载次序defer与async","## 优化的思路","## 排查优化点、衡量网站的性能的方法","## 补充&概念","## 整体大纲"],
       },{
@@ -28,7 +28,7 @@ const article = [{
         fileName: "2eb8add3da46383c06f244240c573a2584e75229",
         title: "HTTP的缓存策略",
         filePath: "/blogs/article/",
-        date: "2021/11/14 上午12:34:37",
+        date: "2021/12/6 下午10:29:46",
         description: "## http1.0 使用强制缓存@@@@@@expires 强制缓存，单位是毫秒，如果客户端跟服务端时间不一致的情况下，会出现bug，前台无法感知@@@@@@```javascript",
         outline: ["## http1.0 使用强制缓存","## http1.1 使用强制缓存与协商缓存"],
       },{
@@ -36,7 +36,7 @@ const article = [{
         fileName: "659ab64bf80576344bc780a7a88dc0bc7b34e127",
         title: "react远程组件实践",
         filePath: "/blogs/article/",
-        date: "2021/12/20 上午9:31:02",
+        date: "2021/12/19 下午7:47:43",
         description: "## react项目加载远程组件@@@@@@  react项目使用远程组件的解决方案，可用来做微前端架构，也可用来解决部分模块的多端公用问题。@@@@@@  目前，我把远程加载组件技术用来解决公司多端使用，且需求易变的功能模块，需多端处理+上线的问题。",
         outline: ["## react项目加载远程组件","## 原理","## 流程","## umd打包模式","### 打包组件示例","### 远程组件使用示例","## amd打包模式","### 打包组件示例","### 远程组件使用示例","## 更多"],
       },{
@@ -44,7 +44,7 @@ const article = [{
         fileName: "8ade966aa30edb50a36c20f7cbfdeda1d902c914",
         title: "v8引擎执行机制",
         filePath: "/blogs/article/",
-        date: "2021/10/4 下午1:34:25",
+        date: "2021/12/6 下午10:29:46",
         description: "## 引擎执行机制@@@@@@  SourceCode -> Abstract Syntax Tree -> Bytecode -> Binary@@@@@@  引擎中通常拥有parser（解析器）、Ignition（解释器）、TurboFan（编译器）、GC(垃圾回收器)",
         outline: ["## 引擎执行机制","## Parser 和 Pre-Parser"],
       },{
@@ -52,7 +52,7 @@ const article = [{
         fileName: "fdee962fd8f23992dcdff3c7f08787759acefef5",
         title: "webpack调优及项目分析",
         filePath: "/blogs/article/",
-        date: "2021/12/20 上午9:31:02",
+        date: "2021/12/19 下午7:47:43",
         description: "## bundle analysis 打包分析@@@@@@  分析打包文件 @@@@@@  安装：`yarn add -D webpack-bundle-analyzer`",
         outline: ["## bundle analysis 打包分析","## code coverage 代码覆盖率","## magic comments 魔法注释","## prefeching/preloading 预加载","## code splitting 代码分割","## lazy loading 懒加载(按需加载)","## uglifyjs-webpack-plugin","## unimport ","## useless-files-webpack-plugin","## 其他方式","## 最后"],
       },{
@@ -60,15 +60,23 @@ const article = [{
         fileName: "7b4b998cd2db77a3542d94dc2820f06c4ea5aaa6",
         title: "windows系统盘空间优化",
         filePath: "/blogs/article/",
-        date: "2021/11/14 上午12:33:02",
+        date: "2021/12/6 下午10:29:46",
         description: "常见的winowsC盘清理方式，一般就是如下几种@@@	1. 第三方工具@@@	2. 磁盘清理@@@	3. C盘扩容@@@	4. 换个硬盘",
         outline: ["## （link）软连接！","## 重点：此方法是可以重复使用的，C盘永远不怕满了。"],
+      },{
+        hash: "e184854edc9ac8cae380d42fec02d2b82821e8be",
+        fileName: "bc8df1e4349b302c8ba7b47fbae6b2d6d3740a6b",
+        title: "《重构》到底讲了啥？",
+        filePath: "/blogs/article/",
+        date: "2021/12/23 下午10:27:02",
+        description: "## 什么是重构@@@这里先给重构下一个定义：改善既有代码的设计。@@@@@@具体来说就是在不改变代码功能行为的情况下，对其内部结构的一种调整。需要注意的是，重构不是代码优化，重构注重的是提高代码的可理解性与可扩展性，对性能的影响可好可坏。而性能优化则让程序运行的更快，当然，最终的代码可能更难理解和维护。@@@",
+        outline: ["## 什么是重构","## 为什么要重构","### 改善程序的内部设计","### 使得代码更容易理解","### 提高开发的速度 && 方便定位错误","## 重构的原则","### 保持当下的编程状态","### 可控制的重构","## 识别代码的臭味道","### 神秘命名","### 重复代码","### 过长函数","### 数据泥团 && 过长参数","### 全局数据","### 发散式变化","### 霰弹式修改","### for 循环语句","### 复杂的条件逻辑 && 合并条件表达式","### 查询函数与修改函数耦合","### 以卫语句(Guard Clauses)取代嵌套条件表达式","## 何时开始重构","### 添加新功能之前","### 完成新功能后或 code review 后","### 难以添加新功能的时候","## 什么时候不该重构","### 重写比重构容易","### 不需要理解该代码片段时","### 未与合作者商量时","## 重构与性能"],
       },{
         hash: "9ceb6545d00595d657e960bd83c44053cace3f01",
         fileName: "e18e36287ec8d3f5886372b367953c0570a258fa",
         title: "前端模块化的前世今生",
         filePath: "/blogs/article/",
-        date: "2021/10/4 下午1:34:25",
+        date: "2021/12/6 下午10:29:46",
         description: "## 前端为什么要用模块@@@@@@* 减少全局变量，有效避免命名污染@@@* 更好的分离和按需加载@@@* 提高复用性和可维护性",
         outline: ["## 前端为什么要用模块","## 模块之初","### 定义各种各样的全局函数","### 使用命名空间 ","### 使用自执行函数（闭包）","### 使用自执行函数添加参数（添加依赖）","### 以上定义模块的方式，有缺陷","## CommonJS","## AMD","## CMD","## es6模块","## ESM 与 CommonJS区别","## 总结"],
       },{
@@ -76,7 +84,7 @@ const article = [{
         fileName: "3f1d9f52f42f367b46fdd80475363d138abdd4bb",
         title: "前端监控方案",
         filePath: "/blogs/article/",
-        date: "2021/10/4 下午1:34:25",
+        date: "2021/12/6 下午10:29:46",
         description: "## 前端监控方案@@@@@@下面两个API配合使用，收集大部分错误信息@@@@@@  * `window.addEventListener('error')`",
         outline: ["## 前端监控方案","## react内置监控方案"],
       },{
@@ -84,7 +92,7 @@ const article = [{
         fileName: "6c2bc4ffbc7f92ab2a23ae31f3a14b5a06f61e4c",
         title: "堆空间和栈空间",
         filePath: "/blogs/article/",
-        date: "2021/10/11 下午2:13:37",
+        date: "2021/12/6 下午10:29:46",
         description: "## js的内存空间@@@@@@在 JavaScript 的执行过程中， 主要有三种类型内存空间，分别是代码空间、栈空间和堆空间。@@@![7tz9q1ngpp](assets/4.png)@@@",
         outline: ["## js的内存空间","## 栈空间和堆空间","## 又是闭包"],
       },{
@@ -92,7 +100,7 @@ const article = [{
         fileName: "3dae20f9d98bb4462216dd64e4c14c262a65d7bb",
         title: "对称加密与非对称加密",
         filePath: "/blogs/article/",
-        date: "2021/10/4 下午1:34:25",
+        date: "2021/12/6 下午10:29:46",
         description: "## 对称加密@@@@@@在对称加密算法中，加密和解密使用的**密钥是相同的**。加密和解密使用的是同一个密钥。@@@@@@优点：算法公开，计算量小，加密速度快，效率高。",
         outline: ["## 对称加密","## 非对称加密","## 总结","## https"],
       },{
@@ -100,7 +108,7 @@ const article = [{
         fileName: "ff707b7508601b5f1e74f1eab53d102ca2bd8525",
         title: "解释型语言与编译型语言",
         filePath: "/blogs/article/",
-        date: "2021/10/4 下午1:34:25",
+        date: "2021/12/6 下午10:29:46",
         description: "# 解释型语言与编译型语言概念与对比@@@@@@## 编译执行（直接执行）@@@@@@> c语言使用命令行 `gcc x.c` 编译出a.out文件，然后执行 `./a.out`即可运行，这是编译执行，编译后的文件可以直接执行",
         outline: ["# 解释型语言与编译型语言概念与对比","## 编译执行（直接执行）","## 解释执行","## 对比"],
       }]
