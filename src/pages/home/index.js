5d7585ef02f627c4973114dd6bd1892cfeb79726
ty6666 <451376882@qq.com>
2021-12-29 13:37:01 +0800

    备案

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -19,6 +19,9 @@ function Home(props) {
             </Switch>
           </Suspense>
         </RightBox>
+        <div>
+          900t.cn © 2021-2027 版权所有 | <a href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备2021046010号</a>
+        </div>
       </div>
     </div>
   )
