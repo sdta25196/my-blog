09e935b1ac5a7bd54259bf19d523d6e1217c9692
sdta25196 <451376882@qq.com>
2022-01-18 21:41:45 +0800

    提交

--- src/assets/static/translate.js
+++ src/assets/static/translate.js
@@ -1,26 +1,26 @@
 
 const translate = [{
-        hash: "5b121818ece144329d085714369ff8a7c29fcd79",
-        fileName: "1e25be1674c4cd784c50192744e34bf411a05d7c",
-        title: "how does JavaScript Work",
-        filePath: "/blogs/translate/",
-        date: "Wed Oct 13 2021 17:21:28 GMT+0800 (中国标准时间)",
-        description: "## How does JavaScript Work?@@@@@@> JavaScript是如何工作的@@@@@@JavaScript is a client-side scripting language and one of the most efficient, commonly used scripting languages. The term .client-side scripting language means that it runs at the client-side( or on the client machine) inside the web-browsers, but one important thing to remember is that client's web-browser also needs to support the JavaScript or it must be JavaScript enabled. Nowadays, most of the modern web browsers support JavaScript and have their JavaScript engines. For example, Google Chrome has its own JavaScript engine called V8.@@@",
-        outline: ["## How does JavaScript Work?","## Introduction ","### Step 1: Parser","### Step 2: AST","### Step 3: Conversion to Machine code","### Step 4: Machine code"],
-      },{
         hash: "4734eb85f0e851ddc244ee63d37faa89f7f59216",
         fileName: "e919341c71771ea9dc14ad522ad2c7eda5227db9",
         title: "Activating Prior Knowledge With English Language Learners",
         filePath: "/blogs/translate/",
-        date: "Wed Oct 13 2021 11:40:36 GMT+0800 (中国标准时间)",
+        date: "Mon Dec 06 2021 22:29:46 GMT+0800 (中国标准时间)",
         description: "## Activating Prior Knowledge With English Language Learners@@@@@@> 激活英语学习者的先验知识@@@@@@Activating prior knowledge means both eliciting from students what they already know and building initial knowledge that they need in order to access upcoming content.@@@",
         outline: ["## Activating Prior Knowledge With English Language Learners"],
+      },{
+        hash: "5b121818ece144329d085714369ff8a7c29fcd79",
+        fileName: "1e25be1674c4cd784c50192744e34bf411a05d7c",
+        title: "how does JavaScript Work",
+        filePath: "/blogs/translate/",
+        date: "Mon Dec 06 2021 22:29:46 GMT+0800 (中国标准时间)",
+        description: "## How does JavaScript Work?@@@@@@> JavaScript是如何工作的@@@@@@JavaScript is a client-side scripting language and one of the most efficient, commonly used scripting languages. The term .client-side scripting language means that it runs at the client-side( or on the client machine) inside the web-browsers, but one important thing to remember is that client's web-browser also needs to support the JavaScript or it must be JavaScript enabled. Nowadays, most of the modern web browsers support JavaScript and have their JavaScript engines. For example, Google Chrome has its own JavaScript engine called V8.@@@",
+        outline: ["## How does JavaScript Work?","## Introduction ","### Step 1: Parser","### Step 2: AST","### Step 3: Conversion to Machine code","### Step 4: Machine code"],
       },{
         hash: "61dcbecbdeeb1f4645939f5e328d81f2eed1e60b",
         fileName: "2e76d0fdfc253b0eda8b4b7493bf05ae2dc38b97",
         title: "What Is Clean Code",
         filePath: "/blogs/translate/",
-        date: "Mon Oct 04 2021 13:45:32 GMT+0800 (中国标准时间)",
+        date: "Mon Dec 06 2021 22:29:46 GMT+0800 (中国标准时间)",
         description: "## What Is Clean Code?@@@@@@> 什么是简洁代码@@@@@@Clean code is a philosophy that I follow when I code. Actually, to me, it feels more like a religion than a philosophy! It’s the idea that your code should be precise and as close to perfect as possible. If you have more code than you need, it shouldn’t be there. There shouldn’t be anything superfluous, and I go so far as to say that there shouldn’t even be superfluous whitespace. You want your code to be as efficient, readable, and maintainable as possible, and instead of only \"solving the problem.\" I always put in the extra time to also focus on the design of my code.@@@",
         outline: ["## What Is Clean Code?","## How to Implement Clean Coding Design","## Estimation Gambling","## System Design","## Writing the Code"],
       }]
