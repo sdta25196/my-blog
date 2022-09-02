63d53b6555356b514c4686f4f1ec5cfa22bdde6e
归零 <451376882@qq.com>
2022-09-02 18:03:42 +0800

    t

---
+++ blog-ssr/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse, userAgent } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  console.log(userAgent(request))
+  console.log('----------------------')
+  // return NextResponse.redirect(new URL('/about-2', request.url))
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/',
+}
--- blog-ssr/pages/index.js
+++ blog-ssr/pages/index.js
@@ -5,8 +5,8 @@ import styles from '../styles/home.module.scss'
 import fs from 'fs'
 import { resolve } from 'path'
 
-export async function getStaticProps(context) {
-
+// export async function getStaticProps({ paths }) {
+export async function getServerSideProps() {
   let a = fs.readFileSync(resolve(__dirname, '../../../../blogs/article/chrome控制台使用.md'), 'utf8')
   // const str = await new Promise(resolve => {
   //   setTimeout(() => {
@@ -22,78 +22,77 @@ export async function getStaticProps(context) {
 
 
 export default function Home({ str }) {
-  console.log(str)
   const [str1, setStr] = useState()
   useEffect(() => {
     setStr(777)
   }, [])
   return (
     <div className={styles.container}>
-      {/* {str} */}
+      {str}
       {str1}
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <Image src='/favicon.ico' width={50} height={50}/>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Image src='/favicon.ico' width={50} height={50} />
     </div>
   )
 }
